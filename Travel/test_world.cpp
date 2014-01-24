23f03e65a825b7f177ebb4cb58997e356db96a22
Cheolha Pedro Lee <leepedro@gmail.com>
2014-01-24 04:09:26 -0500

    updated. broken now.

--- Travel/test_world.cpp
+++ Travel/test_world.cpp
@@ -4,17 +4,17 @@ void TestLLCoordinate(void)
 {
 	using namespace World;
 	LLCoordinate ll1 = { 1, 1 }, ll2 = { -1, -1 }, ll3 = { 181, 181 }, ll4 = { -181, -181 }, ll5 = { 361, 361 }, ll6 = { -361, -361 };
-	ll1.correct();
-	ll2.correct();
-	ll3.correct();
-	ll4.correct();
-	ll5.correct();
-	ll6.correct();
+	//ll1.correct();
+	//ll2.correct();
+	//ll3.correct();
+	//ll4.correct();
+	//ll5.correct();
+	//ll6.correct();
 }
 
 int main(void)
 {
 	TestLLCoordinate();
 	World::Traveler tv1;
-	World::Town t1;
+	World::Town SanJose = { "San Jose", World::dms2dec(37, 20), World::dms2dec(-121, -54) };
 }
--- Travel/traveler.h
+++ Travel/traveler.h
@@ -13,6 +13,54 @@ namespace World
 	class Traveler;
 	//class Town;
 
+	const double PI = std::atan(1) * 4;	// (radian)
+	const double EARTH_RADIUS = 6371;	// (km)
+
+	double degree2rad(double degree)
+	{
+		return degree * PI / 180.0;
+	}
+
+	double rad2degree(double radian)
+	{
+		return radian / PI * 180.0;
+	}
+
+	// Converts DMS {degree, minute, second} to decimal degree.
+	// NOTE: If west, all variables must be negative values.
+	double dms2dec(double degree, double minute, double second = 0.0)
+	{
+		if ((degree >= 0 && minute >= 0 && second >= 0) ||
+			(degree < 0 && minute < 0 && second < 0))
+			return degree + (minute * 60 + second) / 3600;
+		else
+			throw std::logic_error(
+			"The signs of all {degree, minute, second} must be identical.");
+	}
+
+	// Compute the distance (km) between two geo-positions (degree) using Spherical law of
+	// Cosines with mean earth radius.
+	double get_distance(double lat1, double lon1, double lat2, double lon2)
+	{
+		double lat1_r = degree2rad(lat1);
+		double lat2_r = degree2rad(lat2);
+		double diff_lon = degree2rad(lon2 - lon1);
+		using namespace std;
+		return acos(sin(lat1_r) * sin(lat2_r) + cos(lat1_r) * cos(lat2_r) * cos(diff_lon)) *
+			EARTH_RADIUS;
+	}
+
+	double get_bearing(double lat1, double lon1, double lat2, double lon2)
+	{
+		double lat1_r = degree2rad(lat1);
+		double lat2_r = degree2rad(lat2);
+		double diff_lon = degree2rad(lon2 - lon1);
+		using namespace std;
+		double bearing_r = atan2(std::sin(diff_lon) * cos(lat2_r),
+			cos(lat1_r) * sin(lat2_r) - sin(lat1_r) * cos(lat2_r) * cos(diff_lon));
+		return fmod(rad2degree(bearing_r) + 180, 360);	// [-pi, ..., pi] -> [0, ..., 360).
+	}
+
 	class LLCoordinate
 	{
 	public:
@@ -22,17 +70,24 @@ namespace World
 		//double &phi = this->latitude;
 		void correct(void);
 	protected:
-		static double correct_degree(double value);
+		static double correct_latitude(double value);
+		static double correct_longitude(double value);
 	};
 
 	void LLCoordinate::correct(void)
 	{
-		this->latitude = LLCoordinate::correct_degree(this->latitude);
-		this->longitude = LLCoordinate::correct_degree(this->longitude);
+		this->latitude = LLCoordinate::correct_latitude(this->latitude);
+		this->longitude = LLCoordinate::correct_longitude(this->longitude);
 	}
 
-	// Adjusts given value (degree) between (-180, +180].
-	double LLCoordinate::correct_degree(double value)
+	// Adjusts given longitude value (degree).
+	double LLCoordinate::correct_latitude(double value)
+	{
+		throw std::logic_error("Not implemented.");
+	}
+
+	// Limits given longitude value (degree) between (-180, ..., +180].
+	double LLCoordinate::correct_longitude(double value)
 	{
 		auto temp = std::fmod(value, 360);
 		return temp > 180 ? temp - 360 : (temp <= -180 ? temp + 360 : temp);
@@ -41,17 +96,28 @@ namespace World
 	class Town
 	{
 	public:
+		Town(const std::string &name, double latitude, double longitude);
+
 		// properties
 		std::string name;
-		LLCoordinate position;
+		//LLCoordinate coordinates;
+		double &latitude = this->coordinates_.first;
+		double &longitude = this->coordinates_.second;
 
 		// (real-time) status
 		std::list<Traveler> travlers;
+
+	protected:
+		std::pair<double, double> coordinates_;
 	};
+
+	Town::Town(const std::string &name, double latitude, double longitude) : name(name), coordinates_(latitude, longitude) {}
 
 	class Traveler
 	{
 	public:
+		Traveler(void) = default;
+
 		// properties
 		std::string name;
 		double speed;		// distance(degree) / time (day)
@@ -63,12 +129,17 @@ namespace World
 
 		// (real-time) status
 		double speed_now;
-		LLCoordinate position;
+		double bearing_now;
+		//LLCoordinate position;		
+
+		double &latitude = this->position_.first;
+		double &longitude = this->position_.second;
 
 		void move(const LLCoordinate &pos);
 		//LLCoordinate &position = this->position_;
 	protected:
 		//LLCoordinate position_;
+		std::pair<double, double> position_;
 	};
 
 }
